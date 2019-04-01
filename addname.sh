39effdb1d4e66c5dbad2de25f3af295647664349
anandhakrishnan3011 <anandhumohan3011@gmail.com>
2019-04-01 15:02:54 +0530

    intial push

---
+++ addname.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Illegal number of parameters"
+fi
+if [ -f $1 ]
+then
+    echo "the file exists"
+else
+  echo "file not exists"
+fi
+if grep -qF "$2" ulist 
+ then
+   echo "name already exists"
+   else
+    echo "name does not exist"
+    echo "$2">>ulist
+ fi
